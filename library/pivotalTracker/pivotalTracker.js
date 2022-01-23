372a96e53ab5960bce9ad359250acad483ba5a6d
Christian Nogueras <christian@opticpower.com>
2022-01-23 15:44:53 +0000

    Add wip PivotalTracker class

---
+++ library/pivotalTracker/pivotalTracker.js
@@ -0,0 +1,10 @@
+class PivotalTracker {
+    constructor(token, projectId){
+        this.token = token;
+        this.projectId = projectId;
+    }
+    getBlockersOfStory(storyId){
+        // call the fetch endpoint
+        console.log('Calling the fetch endpoint');
+    }
+}
