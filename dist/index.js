86e85cfe95a8c3acbcfb1652428f03f6f69c4e6b
Christian Nogueras <christian@opticpower.com>
2022-01-23 23:06:14 +0000

    Add build

--- dist/index.js
+++ dist/index.js
@@ -5942,42 +5942,20 @@ function wrappy (fn, cb) {
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
 __nccwpck_require__(437).config();
-const fetch = __nccwpck_require__(467);
 const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
-const {PivotalTracker} = __nccwpck_require__(73)
-
-const getBranchName = (eventName, payload) => {
-  let branchName;
-  switch (eventName) {
-      case 'push':
-          branchName = payload.ref.replace('refs/heads/', '');
-          break;
-      case 'pull_request':
-          branchName = payload.pull_request.head.ref;
-          break;
-      default:
-          throw new Error(`Invalid event name when retrieving branch name: ${eventName}`);
-  }
-  return branchName;
-}
-
+const {PivotalTracker} = __nccwpck_require__(73);
+const {getBranchName, getTicketNumberFromBranchName} = __nccwpck_require__(254)
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const PIVOTAL_TOKEN = core.getInput('PIVOTAL_TOKEN') || process.env.PIVOTAL_TOKEN;
   const PROJECT_ID = core.getInput('PROJECT_ID') || process.env.PROJECT_ID;
 
-  const eventName = github.context.eventName;
-  const branchName = getBranchName(eventName, github.context.payload);
-  // const branchName = '180748928-pivotal'
-  const lookForTicketNumberRegex = /[^a-z-.#]\ *([0-9]){7}\d/g;
-  let storyId = branchName.match(lookForTicketNumberRegex);
-  if (storyId) storyId = storyId.toString().trim()
-  console.log(storyId)
-  // const storyId = '180952984' // regex expresion to filter number
-  // const storyId = '180864555' // no blockers
 
+  const branchName = getBranchName(github.context.eventName, github.context.payload);
+  const storyId = getTicketNumberFromBranchName(branchName);
+  
   const Pivotal = new PivotalTracker(PIVOTAL_TOKEN,PROJECT_ID);
   const storyHasBlockers = await Pivotal.storyHasBlockers(storyId);
   if (storyHasBlockers) {
@@ -6010,6 +5988,35 @@ run().catch(e => {
   });
  */
 
+
+/***/ }),
+
+/***/ 254:
+/***/ ((module) => {
+
+const getTicketNumberFromBranchName = (branchName) => {
+    const lookForTicketNumberRegex = /[^a-z-.#]\ *([0-9]){7}\d/g;
+    let storyId = branchName.match(lookForTicketNumberRegex);
+    if (storyId) storyId = storyId.toString().trim()
+}
+const getBranchName = (eventName, payload) => {
+    let branchName;
+    switch (eventName) {
+        case 'push':
+            branchName = payload.ref.replace('refs/heads/', '');
+            break;
+        case 'pull_request':
+            branchName = payload.pull_request.head.ref;
+            break;
+        default:
+            throw new Error(`Invalid event name when retrieving branch name: ${eventName}`);
+    }
+}
+
+module.exports = {
+    getBranchName,
+    getTicketNumberFromBranchName
+}
 
 /***/ }),
 
