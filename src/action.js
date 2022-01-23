3b66f788572cfae1f2e55e6fa9f153d2f4f3e12d
Christian Nogueras <christian@opticpower.com>
2022-01-23 23:04:22 +0000

    Add utility file

--- src/action.js
+++ src/action.js
@@ -1,40 +1,18 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
 const core = require('@actions/core');
 const github = require('@actions/github');
-const {PivotalTracker} = require('../library/')
-
-const getBranchName = (eventName, payload) => {
-  let branchName;
-  switch (eventName) {
-      case 'push':
-          branchName = payload.ref.replace('refs/heads/', '');
-          break;
-      case 'pull_request':
-          branchName = payload.pull_request.head.ref;
-          break;
-      default:
-          throw new Error(`Invalid event name when retrieving branch name: ${eventName}`);
-  }
-  return branchName;
-}
-
+const {PivotalTracker} = require('../library/');
+const {getBranchName, getTicketNumberFromBranchName} = require('./util')
 
 async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const PIVOTAL_TOKEN = core.getInput('PIVOTAL_TOKEN') || process.env.PIVOTAL_TOKEN;
   const PROJECT_ID = core.getInput('PROJECT_ID') || process.env.PROJECT_ID;
 
-  const eventName = github.context.eventName;
-  const branchName = getBranchName(eventName, github.context.payload);
-  // const branchName = '180748928-pivotal'
-  const lookForTicketNumberRegex = /[^a-z-.#]\ *([0-9]){7}\d/g;
-  let storyId = branchName.match(lookForTicketNumberRegex);
-  if (storyId) storyId = storyId.toString().trim()
-  console.log(storyId)
-  // const storyId = '180952984' // regex expresion to filter number
-  // const storyId = '180864555' // no blockers
 
+  const branchName = getBranchName(github.context.eventName, github.context.payload);
+  const storyId = getTicketNumberFromBranchName(branchName);
+  
   const Pivotal = new PivotalTracker(PIVOTAL_TOKEN,PROJECT_ID);
   const storyHasBlockers = await Pivotal.storyHasBlockers(storyId);
   if (storyHasBlockers) {
